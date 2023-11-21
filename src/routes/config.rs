52c193f36daf5df275025042dd55c3701fcaf85e
caioopra <caioprsilva@gmail.com>
2023-11-21 20:35:53 -0300

    create roles services

--- src/routes/config.rs
+++ src/routes/config.rs
@@ -1,14 +1,17 @@
 use actix_web::web;
 use super::game::{create_game, get_games};
 use super::genre::{create_genre, get_genres};
+use super::role::{create_role, get_roles};
 
 
 pub fn config(conf: &mut web::ServiceConfig) {
     let scope = web::scope("/api")
         .service(get_games)
         .service(create_game)
-        .service(create_genre) // Add the `create_genre` service
-        .service(get_genres); // Add the `get_genres` service
+        .service(create_genre) 
+        .service(get_genres) 
+        .service(create_role) 
+        .service(get_roles);
         // .service(delete_game);
 
     conf.service(scope);
